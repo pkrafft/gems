d4f7eb27749ec7716b1e77db103d49cf204a599b
pkrafft <p.m.krafft@gmail.com>
2019-08-22 15:13:18 +0100

    Completed basic demo

--- static/scripts/experiment.js
+++ static/scripts/experiment.js
@@ -1,4 +1,6 @@
 var my_node_id;
+var round = 1;
+var index = 2;
 
 var get_info = function() {
   // Get info for node
@@ -41,15 +43,49 @@ var create_agent = function() {
 $(document).ready(function() {
 
   $("#finish-reading").click(function() {
+
+    //$("#submit-response").hide();
     $("#stimulus").hide();
     $("#response-form").show();
+
     prior = '<select><option selected="selected" disabled>3</option></select>';
-    $("#previous").html('<p>You are a technician in the 5th shift.</p><p><b>A technician from the 4th shift, building on the work of the previous shifts, thought the classification is</b> ' + prior + '.</p>');
-    $("#evidence").html('<b>Your own first lab test shows that the classification is likely A, B, or C.</b>');
+
+    $("#context").html('<p>You are a technician in the 5th shift.</p>');
+
+    $("#previous").html('<p><b>A technician from the 4th shift, building on the work of the previous shifts, thought the classification is</b> ' + prior + '.</p>');
+
+    $("#evidence-1").html('<b>Your own first lab test shows that the classification is likely A, B, or C.</b>');
+
     $("#more").html('<p><b>To see the opinion of another technician from the 4th shift click: </b><button id="resample" type="button" class="btn btn-info">Here</button></p>')
-    $("#submit-response").removeClass('disabled');
-    $("#submit-response").html('Submit');
+
+
+    $("#resample").click(function() {
+      $("#previous").hide();
+
+      index += 1
+      prior = '<select><option selected="selected" disabled>' + index + '</option></select>';
+
+      $("#nextsample").html('<p><b>A technician you ask from the 4th shift, building on the work of the previous shifts, thinks the classification is</b> ' + prior + '.</p>');
+    });
+
+
   });
+
+  $("#continue").click(function() {
+
+    round += 1;
+
+    if(round <= 4) {
+      $("#evidence-" + round + "").html('<b>Your own test ' + round + ' shows that the classification is likely A, B, or C.</b>');
+    } else {
+      $("#continue").addClass('disabled');
+      //$("#submit-response").show();
+      $("#submit-response").prop('disabled', false);
+      //$("#submit-response").html('Submit');
+    }
+
+  });
+
 
   $("#submit-response").click(function() {
     $("#submit-response").addClass('disabled');

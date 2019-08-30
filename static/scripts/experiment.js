ff1bf66066a9768f390682925e5a1fd9d3691ef3
pkrafft <p.m.krafft@gmail.com>
2019-08-30 15:09:19 -0700

    Working front end

--- static/scripts/experiment.js
+++ static/scripts/experiment.js
@@ -2,7 +2,7 @@ var my_node_id;
 var round = 1;
 var index = 0;
 var count = 0;
-var folds = ['previous', 'your-lab', 'your-choice', 'END'];
+var folds = ['#context', '#previous', '#your-lab', '#your-choice', 'END'];
 
 var get_info = function() {
   // Get info for node
@@ -11,9 +11,6 @@ var get_info = function() {
       var choice = resp.infos[0].contents;
       $('#loading').html('');
       $("#graphic").attr('src', '/static/images/berry-1.png');
-      $("#stimulus").show();
-      $("#response-form").hide();
-      $("#finish-reading").show();
     })
     .fail(function (rejection) {
       console.log(rejection);
@@ -23,10 +20,10 @@ var get_info = function() {
 
 // Create the agent.
 var create_agent = function() {
-  $('#finish-reading').prop('disabled', true);
   dallinger.createAgent()
     .done(function (resp) {
-      $('#finish-reading').prop('disabled', false);
+      $('#continue').html('Continue');
+      $('#continue').prop('disabled', false);
       my_node_id = resp.node.id;
       get_info();
     })
@@ -44,13 +41,58 @@ var create_agent = function() {
 // Consent to the experiment.
 $(document).ready(function() {
 
-  $("#finish-reading").click(function() {
 
-    //$("#submit-response").hide();
-    $("#stimulus").hide();
-    $("#response-form").show();
+  $("#continue").click(function() {
+    next();
+  });
 
-    $("#continue").prop('disabled', false);
+
+  $("#submit-response").click(function() {
+    if(round == 4) {
+      $("#submit-response").addClass('disabled');
+      $("#submit-response").html('Sending...');
+
+      var response = $("#classification").val();
+
+      $("#classification").disabled = true;
+
+      dallinger.createInfo(my_node_id, {
+        contents: response,
+        info_type: 'Info'
+      }).done(function (resp) {
+        create_agent();
+      });
+    } else {
+
+      index -= 1;
+      round += 1;
+
+      $(folds[index]).hide();
+
+      if(round <= 4) {
+
+        var text = '';
+        text += '<div style="display: inline" class="update"><font color="red"><b>Update!</b></font> </div>';
+        text += '<b>Your own test ' + round + ' shows that the classification is likely A, B, or C.</b>';
+
+        $("#evidence-" + round + "").html(text);
+
+        //setTimeout(clearUpdates(), 3000)
+      }
+
+      $('#continue').show();
+      $("#continue").prop('disabled', false);
+      $("#submit-response").prop('disabled', true);
+
+    }
+  });
+
+});
+
+var next = function() {
+
+  clearUpdates();
+  hideSample();
 
     prior = '<select><option selected="selected" disabled>3</option></select>';
 
@@ -60,50 +102,17 @@ $(document).ready(function() {
 
     $("#evidence-1").html('<b>Your own first lab test shows that the classification is likely A, B, or C.</b>');
 
-  });
-
-  $("#continue").click(function() {
-
-    if (folds[index] == 'END') {
-
-      round += 1;
-
-      if(round <= 4) {
-        $("#evidence-" + round + "").html('<b>Your own test ' + round + ' shows that the classification is likely A, B, or C.</b>');
-      } else {
-        $("#continue").addClass('disabled');
-        //$("#submit-response").show();
-        $("#submit-response").prop('disabled', false);
-        //$("#submit-response").html('Submit');
-      }
-    } else {
-
-      var tab = '#' + folds[index];
-      $(tab).show();
-
+    if(folds[index] != 'END') {
+      $(folds[index]).show();
       index += 1
     }
 
-  });
-
-
-  $("#submit-response").click(function() {
-    $("#submit-response").addClass('disabled');
-    $("#submit-response").html('Sending...');
-
-    var response = $("#classification").val();
-
-    $("#classification").disabled = true;
-
-    dallinger.createInfo(my_node_id, {
-      contents: response,
-      info_type: 'Info'
-    }).done(function (resp) {
-      create_agent();
-    });
-  });
-
-});
+    if (folds[index] == 'END') {
+      $("#continue").prop('disabled', true);
+      $('#continue').hide();
+      $("#submit-response").prop('disabled', false);
+    }
+}
 
 var resample = function() {
 
@@ -115,15 +124,28 @@ var resample = function() {
 
       prior = '<select><option selected="selected" disabled>' + 0 + '</option></select>';
 
-      var text = 'A technician you ask from the 4th shift, building on the work of the previous shifts, thinks the classification is ' + prior + '. ';
+        var text = '';
+        text += '<div style="display: inline" class="update"><font color="red"><b>Update!</b></font> </div>';
+      text += 'A technician you ask from the 4th shift, building on the work of the previous shifts, thinks the classification is ' + prior + '. ';
       text += '<button id="hide-sample" type="button" class="btn btn-info btn-sm">HIDE</button></p>';
 
       $('#nextsample').show();
       $("#nextsample").html(text);
 
       $("#hide-sample").click(function() {
-        $('#nextsample').hide();
-        $("#resample-text").show();
+        hideSample();
       });
 
     }
+
+var hideSample = function() {
+        clearUpdates();
+        $('#nextsample').hide();
+        $("#resample-text").show();
+}
+
+var clearUpdates = function() {
+  $('.update').hide()
+  $('.update').removeClass('update')
+  $('.update').show()
+}
